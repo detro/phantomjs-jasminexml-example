78fc99aa96914791c601488ff4f033a194f48af9
Ivan De Marino <ivan.de.marino@gmail.com>
2011-06-16 16:29:45 +0100

    * Adding "volume" functionality to the TV  :)

--- src/tv.js
+++ src/tv.js
@@ -1,6 +1,7 @@
 function Tv() {
     var _currentChannel = "BBC 1",
-        _on = false;
+        _on = false,
+        _currentVolume = 50;
     
     var _setChannel = function(newChannel) {
         if ( !_on ) {
@@ -33,11 +34,28 @@ function Tv() {
         return false;
     };
     
+    var _volume = function() {
+        if ( !_on ) {
+            return false;
+        }
+        return _currentVolume;
+    };
+    
+    var _setVolume = function(newVolume) {
+        if ( _on ) {
+            _currentVolume = newVolume;
+            return true;
+        }
+        return false;
+    };
+    
     return {
         setChannel : _setChannel,
         channel : _channel,
         turnOn : _turnOn,
         turnOff : _turnOff,
+        setVolume : _setVolume,
+        volume : _volume,
         isOn : function(){ return _on; }
     };
 };
